06fbfcce1017449ba2484cfc9296d5e961b47386
eglushkov <eugeniyglushkov@gmail.com>
2021-03-19 14:40:36 +0300

    MNR-17: add TODO comment

--- mobile-news-reader/SearchNewsModule/Presenter/SearchNewsPresenterProtocol.swift
+++ mobile-news-reader/SearchNewsModule/Presenter/SearchNewsPresenterProtocol.swift
@@ -11,5 +11,6 @@ import UIKit
 protocol SearchNewsPresenterProtocol: AnyObject {
     init(view: SearchNewsViewProtocol, service: SearchNewsServiceProtocol, netService: NetServiceProtocol)
     func searchNews(forString string: String)
+    //TODO: - need extract protocol with loadImages for this protocol and NewsPresenter
     func loadImages(news: [NewTO])
 }
